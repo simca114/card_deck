c975fe47e1d9c1750992645808f8d6542d054eec
Alexander Simchuk <alsim114@gmail.com>
2016-12-30 18:52:50 -0800

    created configuration class to define valid card suits and values

---
+++ classes/config.js
@@ -0,0 +1,12 @@
+function CardDefinition() {
+    var suits = ["heart", "diamond", "club", "spade"];
+    var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+    this.getSuits = function () {
+        return suits;
+    };
+
+    this.getValues = fuction () {
+        return values;
+    };
+}
