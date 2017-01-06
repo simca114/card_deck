70b8a0dd33611d409011cbfe7edbded65ec0ef21
Alexander <alsim114@gmail.com>
2017-01-05 23:49:32 -0800

    Deck.shuffle() tests written

--- tests/deck_tests.js
+++ tests/deck_tests.js
@@ -36,3 +36,53 @@ QUnit.test( "Test if new Deck has exactly 52 cards", function (assert) {
         "A newly created deck has 52 cards"
     );
 });
+
+/* TODO: explain what is being tested here and why
+ *
+ */
+QUnit.test( "Test if Deck.shuffle() changes the order of the cards without destroying any cards (multiple runthroughs)", function (assert) {
+    for (deckNum = 1; deckNum <= 5; deckNum++) {
+        var deck = new Deck();
+
+        for (timesShuffled = 1; timesShuffled <= 10; timesShuffled++) {
+            var messagePrefix = "Deck " + deckNum.toString() + " shuffled " + timesShuffled.toString() + " time(s),";
+            var oldCardOrder = deck.getCards();
+
+            deck.shuffle();
+            var newCardOrder = deck.getCards();
+            // deck still has 52 cards post shuffle
+            assert.ok(
+                newCardOrder.length == 52,
+                messagePrefix + " still has 52 cards"
+            );
+
+            // deck still has 52 unique, valid cards post shuffle
+            var uniqueRelativeValues = [];
+
+            deck.newCardOrder.forEach(function (card) {
+                var currentRelVal = card.relativeValue();
+                if ($.inArray(currentRelVal, uniqueRelativeValues) == -1) {
+                    uniqueRelativeValues.push(currentRelVal);
+                }
+            });
+            assert.ok(
+                uniqueRelativeValues.length == 52,
+                messagePrefix + " still has 52 unique cards"
+            );
+
+            // at least 1 card position is different post shuffle
+            var orderIsNotEqual = false;
+
+            for(index = 0; index < newCardOrder.length; index++) {
+                if ( oldCardOrder[index].relativeValue() != newCardOrder[index].relativeValue() ) {
+                    orderIsNotEqual = true;
+                    break;
+                }
+            }
+            assert.ok(
+                orderIsNotEqual,
+                messagePrefix + " has at least 1 card in a different position of the deck"
+            );
+        }
+    }
+});
