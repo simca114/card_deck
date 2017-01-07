c4e30be1022032ec9e3484e239b7f0df8ffd35d4
Alexander <alsim114@gmail.com>
2017-01-06 22:29:40 -0800

    Deck.sort() tests written

--- tests/deck_tests.js
+++ tests/deck_tests.js
@@ -95,3 +95,62 @@ QUnit.test( "Test if Deck.shuffle() changes the order of the cards without destr
         }
     }
 });
+
+/* TODO: explain what is being tested here and why
+ *
+ */
+QUnit.test( "Test if Deck.sort() orders of the cards in ascending order without destroying any cards (multiple runthroughs). NOTE: uses Deck.shuffle() to rescramble card order", function (assert) {
+    for (deckNum = 1; deckNum <= 5; deckNum++) {
+        var deck = new Deck();
+
+        for (timesSorted = 1; timesSorted <= 10; timesSorted++) {
+            var messagePrefix = "Deck " + deckNum.toString() + " shuffled then sorted " + timesSorted.toString() + " time(s),";
+
+            deck.shuffle();
+            deck.sort();
+
+            // deck still has 52 cards post shuffle
+            assert.ok(
+                deck.getCards().length == 52,
+                messagePrefix + " still has 52 cards"
+            );
+
+            // deck still has 52 unique, valid cards post shuffle
+            var uniqueRelativeValues = [];
+
+            deck.getCards().forEach(function (card) {
+                var currentRelVal = card.relativeValue();
+                if ($.inArray(currentRelVal, uniqueRelativeValues) == -1) {
+                    uniqueRelativeValues.push(currentRelVal);
+                }
+            });
+            assert.ok(
+                uniqueRelativeValues.length == 52,
+                messagePrefix + " still has 52 unique cards"
+            );
+
+            // the first card in the deck has the relative value of 1
+            assert.ok(
+                deck.getCards()[0].relativeValue() == 1,
+                messagePrefix + " first card has a relative value of 1"
+            );
+
+            // for the second card till the end, each card has a relative value greater than the previous one by exactly one
+            // we can use 0 as the initial offset because the previous test checks if the first card has a relative value of 1
+            var previousValue = 0;
+            var sorted = true;
+            deck.getCards().forEach(function (card) {
+                var difference = card.relativeValue() - previousValue;
+                if (difference == 1) {
+                    previousValue++;
+                } else {
+                    sorted = false;
+                }
+            });
+            assert.ok(
+                sorted,
+                messagePrefix + " every card after the first one has a relative value greater than the previous one by exactly one"
+            );
+        }
+    }
+});
