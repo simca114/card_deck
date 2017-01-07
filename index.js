934cf57ae3980b915acb7c2df678569d4b2bfdbf
Alexander <alsim114@gmail.com>
2017-01-06 23:21:06 -0800

    applied sort and shuffle methods to their respective buttons

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 $(document).ready(function () {
     var deck;
-    $('#new').click(function () {
+
+    var renderDeck = function (newDeck) {
         var deckDiv = $('#deck_display');
-        if (deck) {
-            deckDiv.empty();
-        }
-        deck = new Deck();
+        deckDiv.empty();
 
         deck.getCards().forEach(function (card) {
             var imgSource = card.imgName();
 
             var img = $(document.createElement('img'));
             img.attr('src', "img/cards/" + imgSource);
+            img.attr('height', 100);
+            img.attr('width', 60);
             img.appendTo(deckDiv);
         });
+
+    }
+
+    $('#new').click(function () {
+        deck = new Deck();
+        renderDeck(deck);
+    });
+    $('#sort').click(function () {
+        deck.sort();
+        renderDeck(deck);
+    });
+    $('#shuffle').click(function () {
+        deck.shuffle();
+        renderDeck(deck);
     });
 });
